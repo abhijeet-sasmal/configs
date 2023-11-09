eded429b1254c922db0fd855227fefb948a89ee4
Abhijeet Sasmal <abhijeet.work.1512@gmail.com>
2023-11-10 02:37:49 +0530

    Font added

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+echo "[-] Download fonts [-]"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/RobotoMono.zip
+unzip RobotoMono.zip -d ~/.fonts
+fc-cache -fv
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
